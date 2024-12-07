9f3346882450fdd9bf0bbfce3c848dccb93b7af3
alexmerlo0623 <a.merlo@ufl.edu>
2024-12-07 17:26:16 -0500

    added CORS

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,28 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const mongoose = require('mongoose')
-const userRoutes = require('./routes/user')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors'); // Import cors
+const userRoutes = require('./routes/user');
 const eventRoutes = require('./routes/event');
 const classRoutes = require('./routes/classroom');
 
 // express app
-const app = express()
+const app = express();
+
+// Enable CORS
+app.use(cors());
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
-app.use('/api/user', userRoutes)
+app.use('/api/user', userRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api/classes', classRoutes);
 
@@ -27,9 +31,9 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT || 3001, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
-    })
+      console.log('connected to db & listening on port', process.env.PORT);
+    });
   })
   .catch((error) => {
-    console.log(error)
-  })
+    console.log(error);
+  });
